c0f89e0f4f2027267a73dcfde3dce02394b87eca
YeeChuen <tychuen98@gmail.com>
2022-11-12 17:58:13 -0600

    update node.js 5.58pm

--- app.js
+++ app.js
@@ -3,17 +3,19 @@ const app = express();
 const path = require('path');
 const router = express.Router();
 
+const bodyParser = require('body-parser');
+
 const mongoose=require('mongoose');
 
 const mongoURL='mongodb+srv://admin:adminpass@alphacluster.8ddek3r.mongodb.net/?retryWrites=true&w=majority';
 
 mongoose.connect(mongoURL, {useNewUrlParser: true, useUnifiedTopology: true },
     () => {
-      console.log("Connected to MongoDB");
+    console.log("Connected to MongoDB");
     }
 );
 
-app.use(express.json);
+app.use(bodyParser.urlencoded({ extended: true }));
 
 router.get('/',function(req,res){
   res.sendFile(path.join(__dirname+'/frontend/index.html'));
@@ -30,7 +32,21 @@ router.get('/sitemap',function(req,res){
 
 router.get('/form',function(req,res){
     res.sendFile(path.join(__dirname+'/frontend/form.html'));
-  });
+});
+
+router.get('/test',function(req,res){
+    res.sendFile(path.join(__dirname+'/frontend/test.html'));
+});
+
+  
+router.post('/form', function (req, res) {
+    const input = req.body
+    console.log(input.user_password)
+    res.sendFile(path.join(__dirname+'/frontend/input.html', 
+    {    user_name: input.user_name,
+        user_password: input.user_password }));
+
+});
 
 //add the router
 app.use('/', router);
